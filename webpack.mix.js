93ceac9a54050cc49fc74b09e95f9aca289def5c
Alejandro González Reyes <alexglzrys@gmail.com>
2020-01-12 20:01:27 -0600

    Add libraries and JS frameworks using NPM and Laravel Mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +11,10 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+// 1. Instalar todos los modulos de NPM registrados en package.json -- npm i
+// 2. Ejecutar el comando -- npm run watch
+
+mix.js("resources/js/app.js", "public/js").sass(
+    "resources/sass/app.scss",
+    "public/css"
+);
